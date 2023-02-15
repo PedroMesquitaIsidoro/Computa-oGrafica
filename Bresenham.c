6c70c63849af01d3b036fbf6923c8d9a0ea23f37
PedroMesquitaIsidoro <83170114+PedroMesquitaIsidoro@users.noreply.github.com>
2023-02-15 16:39:57 -0300

    Update Bresenham.c

--- Bresenham.c
+++ Bresenham.c
@@ -6,7 +6,7 @@ void ligarLinhas(int x0, int y0, int x1, int y1){
     int dy = abs(y1 - y0);
     int sIncrementoX = 0;
     int sIncrementoY = 0;
-    int erro = dx - dy;
+    int erro = (2*dy) - dx;
     int x = x0;
     int y = y0;
 
@@ -26,18 +26,18 @@ void ligarLinhas(int x0, int y0, int x1, int y1){
         sIncrementoY = -1;
     }
 
-    while( x != x1 || y != y1){
+    while( x != x1 && y != y1){
         printf("(%d,%d)\n", x, y);
-        int erro2 = 2 * erro;
 
-        if(erro2 > -dy){
-            erro = erro - dy;
+        if(erro < 0){
+            erro = erro + 2 * dy;
             x = x + sIncrementoX;
         }
 
-        if( erro2 < dx){
-            erro = erro + dx;
+        else{
+            erro = erro + (2 * dy) - ( 2 * dx );
             y = y + sIncrementoY;
+            x = x + sIncrementoX;
         }
     }
     printf("(%d,%d)\n", x, y);
